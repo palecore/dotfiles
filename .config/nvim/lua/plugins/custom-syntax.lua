50fc88a3348110e2a256a7269e84905bd5d76e93
palecore <208073224+palecore@users.noreply.github.com>
2025-05-09 17:32:36 +0200

    add a plugin file for syntax highlighting with treesitter

---
+++ .config/nvim/lua/plugins/custom-syntax.lua
@@ -0,0 +1,58 @@
+---Custom syntax-processing plugins.
+
+---@type LazySpec[]
+return {
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		opts = {
+			max_lines = 3,
+			min_window_height = 16,
+			on_attach = function()
+				vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true })
+			end,
+		},
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = { "custom-helpers" },
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				auto_install = false,
+				ignore_install = {},
+				modules = {},
+				sync_install = false,
+				ensure_installed = {
+					"bash",
+					"jq",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"yaml",
+					"vimdoc", -- Without this, huge "Impossible Query" errors on vim docs.
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						node_incremental = "<c-n>",
+						node_decremental = "<c-p>",
+					},
+				},
+			})
+			-- Highlight vimwiki files as markdown:
+			vim.treesitter.language.register("markdown", { "vimwiki" })
+			-- Explicitly highlight variables as identifiers:
+			CustomHelpers.on_lazy_done(
+				function() vim.api.nvim_set_hl(0, "@variable", { link = "Identifier" }) end
+			)
+		end,
+	},
+}
