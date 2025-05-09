499329bcc813dd5642eee6ee8276aef50b3f6e68
palecore <208073224+palecore@users.noreply.github.com>
2025-05-09 17:33:49 +0200

    add a plugin file for auto-completion with blink.nvim

---
+++ .config/nvim/lua/plugins/custom-completion.lua
@@ -0,0 +1,47 @@
+---@type LazySpec
+return {
+	"saghen/blink.cmp",
+	version = "1.2.0",
+	dependencies = {
+		-- For helper utils:
+		"custom-helpers",
+	},
+	config = function()
+		require("blink.cmp").setup({ ---@type blink.cmp.Config
+			keymap = {
+				["<CR>"] = { "accept", "fallback" },
+				["<C-e>"] = { "hide", "fallback" },
+				["<C-p>"] = { "select_prev", "fallback" },
+				["<C-n>"] = { "select_next", "fallback" },
+			},
+			cmdline = {
+				keymap = {
+					["<CR>"] = { "accept_and_enter", "fallback" },
+					["<C-e>"] = { "hide", "fallback" },
+				},
+				completion = {
+					list = { selection = { preselect = false } },
+					menu = { auto_show = true },
+				},
+			},
+			completion = {
+				menu = {
+					draw = {
+						treesitter = { "lsp" },
+						columns = {
+							{ "label", "label_description", gap = 1 },
+							{ "kind" },
+						},
+					},
+				},
+				documentation = { auto_show = true },
+				ghost_text = { enabled = true },
+			},
+			signature = { enabled = true },
+		})
+		-- Highlight kinds of menu items like keywords:
+		CustomHelpers.on_lazy_done(
+			function() vim.api.nvim_set_hl(0, "BlinkCmpKind", { link = "Keyword" }) end
+		)
+	end,
+}
