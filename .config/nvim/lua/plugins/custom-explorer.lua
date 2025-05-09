d7235aad2baeffd0c96e7d5e5ffbc0fc38ed7b7a
palecore <208073224+palecore@users.noreply.github.com>
2025-05-09 17:29:22 +0200

    add a plugin file for file exploration with oil.nvim

---
+++ .config/nvim/lua/plugins/custom-explorer.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+	"stevearc/oil.nvim",
+	cmd = "Oil",
+	keys = {
+		{ "-", function() require("oil").open() end, id = "oil_open_parent_dir" },
+	},
+	opts = {
+		skip_confirm_for_simple_edits = true,
+		columns = { "permissions", "size", "mtime" },
+		view_options = { show_hidden = true },
+	},
+}
