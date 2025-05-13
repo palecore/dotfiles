a7cc82a369a41a8967ea082785f5fd51f178d16e
palecore <208073224+palecore@users.noreply.github.com>
2025-05-13 20:29:00 +0200

    enable oil.nvim as the default file explorer

--- .config/nvim/lua/plugins/custom-explorer.lua
+++ .config/nvim/lua/plugins/custom-explorer.lua
@@ -6,6 +6,7 @@ return {
 		{ "-", function() require("oil").open() end, id = "oil_open_parent_dir" },
 	},
 	opts = {
+		default_file_explorer = true,
 		skip_confirm_for_simple_edits = true,
 		columns = { "permissions", "size", "mtime" },
 		view_options = { show_hidden = true },
