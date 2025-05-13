5779e971ca7e7b1c51f4132ffb3cd402f44c1a7a
palecore <208073224+palecore@users.noreply.github.com>
2025-05-13 20:26:22 +0200

    rewrite custom-actions as custom-action-store
    The alternative approach relies on the vim-like precedence: buffer vs
    window vs tabpage vs filetype vs global. It also shall not hardcode the
    action names, but instead only provide the behavior.

---
+++ .config/nvim/lua/custom-action-store.lua
@@ -0,0 +1,80 @@
+---Aggregates custom actions polymorhic depending on the context they're
+---executed in.
+local M = {}
+
+local action_store = {
+	---@type table<integer, table<string, function>>
+	buffer = {},
+	---@type table<integer, table<string, function>>
+	window = {},
+	---@type table<integer, table<string, function>>
+	tabpage = {},
+	---@type table<string, table<string, function>>
+	filetype = {},
+	---@type table<string, function>
+	global = {},
+}
+
+---Executes the action
+---@param action_name custom_action_store.ActionName
+---@param payload? table for future use
+---@return nil result for future use
+function M.exec_action(action_name, payload)
+	local bufnr = vim.api.nvim_get_current_buf()
+	local buf_action = (action_store.buffer[bufnr] or {})[action_name]
+	if type(buf_action) == "function" then return buf_action(payload) end
+	--
+	local winnr = vim.api.nvim_get_current_win()
+	local win_action = (action_store.window[winnr] or {})[action_name]
+	if type(win_action) == "function" then return win_action(payload) end
+	--
+	local tab = vim.api.nvim_get_current_tabpage()
+	local tab_action = (action_store.tabpage[tab] or {})[action_name]
+	if type(tab_action) == "function" then return tab_action(payload) end
+	--
+	local ft = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+	local ft_action = (action_store.filetype[ft] or {})[action_name]
+	if type(ft_action) == "function" then return ft_action(payload) end
+	--
+	local global_action = action_store.global[action_name]
+	if type(global_action) == "function" then return global_action(payload) end
+	--
+	vim.notify("Unset action '" .. action_name .. "'!", vim.log.levels.ERROR)
+	return nil
+end
+
+---@alias custom_action_store.ActionName
+---| string
+
+---@alias custom_action_store.Scope
+---| { buffer: integer }
+---| { window: integer }
+---| { tabpage: integer }
+---| { filetype: string }
+---| nil
+
+---@alias custom_action_store.ActionFn
+---| fun()
+
+---@param action_name custom_action_store.ActionName
+---@param action_fn custom_action_store.ActionFn
+---@param scope custom_action_store.Scope
+function M.set_action(action_name, action_fn, scope)
+	-- TODO set up autocmds to clean these up on buf/win/tab destruction
+	-- TODO: provide fallback function if overwriting
+	if scope and scope.buffer then
+		local buf_actions = action_store.buffer[scope.buffer] or {}
+		buf_actions[action_name] = action_fn
+		action_store.buffer[scope.buffer] = buf_actions
+	elseif scope and scope.window then
+		action_store.window[scope.window][action_name] = action_fn
+	elseif scope and scope.tabpage then
+		action_store.tabpage[scope.tabpage][action_name] = action_fn
+	elseif scope and scope.filetype then
+		action_store.filetype[scope.filetype][action_name] = action_fn
+	else
+		action_store.global[action_name] = action_fn
+	end
+end
+
+return M
