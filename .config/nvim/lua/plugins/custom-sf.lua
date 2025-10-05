d4098255b788a57d03e45fe09ec3e646effa8bc6
palecore <208073224+palecore@users.noreply.github.com>
2025-10-05 20:41:29 +0200

    (nvim sf) take sf logic out of lazy plugin spec
    Fake /dev/null lazy plugins appear to not be working across all versions
    of neovim.

--- .config/nvim/lua/plugins/custom-sf.lua
+++ .config/nvim/lua/plugins/custom-sf.lua
@@ -1,16 +1,13 @@
 ---Salesforce-development-specific configuration
----@type LazySpec
-return {
-	name = "custom-sf",
-	dir = "/dev/null",
-	config = function()
-		-- Various Salesforce metadata components have non-conventional extensions:
-		vim.filetype.add({
-			extension = {
-				design = "xml",
-				component = "xml",
-				trigger = "apex",
-			},
-		})
-	end,
-}
+
+-- Various Salesforce metadata components have non-conventional extensions:
+vim.filetype.add({
+	extension = {
+		design = "xml",
+		component = "xml",
+		trigger = "apex",
+	},
+})
+
+---@type LazySpec[]
+return {}
