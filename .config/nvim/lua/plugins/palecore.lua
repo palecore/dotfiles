4177a3153441767140dbcd77d70730223b032dbd
palecore <208073224+palecore@users.noreply.github.com>
2025-12-19 22:20:55 +0100

    feat(nvim/github/copilot): add lazy plugin configuration

--- .config/nvim/lua/plugins/palecore.lua
+++ .config/nvim/lua/plugins/palecore.lua
@@ -1,3 +1,8 @@
+local function gh_buf_toggle() return require("palecore.gh-copilot").buf_toggle() end
+local function gh_global_disable()
+	return require("palecore.gh-copilot").global_disable()
+end
+
 ---@type LazySpec[]
 return {
 	{
@@ -14,5 +19,32 @@ return {
 			{ "S", "<Plug>(leap-from-window)", mode = { "n" }, id = "leap_from_window" },
 		},
 		opts = {},
+	},
+	{
+		"github/copilot.vim",
+		cmd = "Copilot",
+		init = function()
+			-- by default, disable copilot for all filetypes:
+			vim.g.copilot_filetypes = { ["*"] = false }
+			vim.g.copilot_no_tab_map = true
+		end,
+		keys = {
+			-- only enable it explicitly per buffer:
+			{ "<leader>ta", gh_buf_toggle, id = "toggle_ai", desc = "Toggle GenAI" },
+			{ "<leader>tA", gh_global_disable, id = "disable_ai1", desc = "Disable GenAI" },
+			{ "<leader>Ta", gh_global_disable, id = "disable_ai2", desc = "Disable GenAI" },
+			{ "<leader>TA", gh_global_disable, id = "disable_ai3", desc = "Disable GenAI" },
+			{
+				"<c-l>",
+				function() return vim.fn["copilot#Accept"]("\r") end,
+				expr = true,
+				replace_keycodes = false,
+				mode = "i",
+				id = "copilot_accept",
+			},
+			{ "<c-h>", "<Plug>(copilot-dismiss)", mode = "i", id = "copilot_dismiss" },
+			{ "<c-p>", "<Plug>(copilot-previous)", mode = "i", id = "copilot_previous" },
+			{ "<c-n>", "<Plug>(copilot-next)", mode = "i", id = "copilot_next" },
+		},
 	},
 }
