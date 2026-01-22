d13e353f11c7d8dacf1ef25a3bcdfe7504909ab6
palecore <208073224+palecore@users.noreply.github.com>
2026-01-23 00:23:44 +0100

    feat: ignore non-dotfile home-root directories except for ~/bin/

---
+++ .config/yadm/alt/bin/termux-url-opener##u.u0_a213,e.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# shellcheck shell=sh
+set -eu
+. ~/.profile
+
+# get_song SONG_URL - download a given song using yt-dlp
+song_url="$1"
+shift
+curr_year="$(date +%Y)"
+music_dir="$XDG_MUSIC_DIR/storage/music/pub/prv/$curr_year"
+mkdir -p "$music_dir"
+python3 -m get_song --directory "$music_dir" "$song_url" &
