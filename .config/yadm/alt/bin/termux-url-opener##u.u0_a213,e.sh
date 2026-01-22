31e160955be73096cda668f51278077682933ef8
palecore <208073224+palecore@users.noreply.github.com>
2026-01-23 00:27:21 +0100

    feat: termux-url-opener: add with initial song-getting functionality

--- .config/yadm/alt/bin/termux-url-opener##u.u0_a213,e.sh
+++ .config/yadm/alt/bin/termux-url-opener##u.u0_a213,e.sh
@@ -4,9 +4,9 @@ set -eu
 . ~/.profile
 
 # get_song SONG_URL - download a given song using yt-dlp
-song_url="$1"
+song_url="${1:?SONG_URL null or empty}"
 shift
 curr_year="$(date +%Y)"
-music_dir="$XDG_MUSIC_DIR/storage/music/pub/prv/$curr_year"
+music_dir="$HOME/storage/music/pub/prv/$curr_year"
 mkdir -p "$music_dir"
 python3 -m get_song --directory "$music_dir" "$song_url" &
