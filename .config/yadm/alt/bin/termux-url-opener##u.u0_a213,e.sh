d25fe413c51170ff66170d7ae8afda0b66a72926
palecore <208073224+palecore@users.noreply.github.com>
2026-01-23 01:14:28 +0100

    fix(termux-url-opener): run get-song in bg using termux-job-scheduler

--- .config/yadm/alt/bin/termux-url-opener##u.u0_a213,e.sh
+++ .config/yadm/alt/bin/termux-url-opener##u.u0_a213,e.sh
@@ -3,10 +3,34 @@
 set -eu
 . ~/.profile
 
-# get_song SONG_URL - download a given song using yt-dlp
 song_url="${1:?SONG_URL null or empty}"
-shift
+
+# get_song SONG_URL - download a given song using yt-dlp
+#
+# NOTE: logic is written to a temporary script which is then run in the
+# background with termux-job-scheduler
+tmp_script_path="$(mktemp)"
+cat > "$tmp_script_path" << 'SH'
+#!/bin/sh
+# shellcheck shell=sh
+set -eu
+. ~/.profile
+
+trap 'rm "$0"' EXIT # the temp script should remove itself afterwards
+
+read -r song_url << 'EOF'
+SH
+
+printf %s\\n "$song_url" >> "$tmp_script_path"
+
+cat >> "$tmp_script_path" << 'SH'
+EOF
 curr_year="$(date +%Y)"
 music_dir="$HOME/storage/music/pub/prv/$curr_year"
 mkdir -p "$music_dir"
-python3 -m get_song --directory "$music_dir" "$song_url" &
+python3 -m get_song --directory "$music_dir" "$song_url"
+SH
+
+chmod +x "$tmp_script_path"
+
+termux-job-scheduler --script "$tmp_script_path"
