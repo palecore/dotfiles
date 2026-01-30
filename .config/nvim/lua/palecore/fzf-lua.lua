74bfb0a3c8a0d882542f80e155682a5fb289f699
palecore <208073224+palecore@users.noreply.github.com>
2026-01-31 00:18:48 +0100

    fix(fzf-lua): add custom ext_filetypes default action handler

--- .config/nvim/lua/palecore/fzf-lua.lua
+++ .config/nvim/lua/palecore/fzf-lua.lua
@@ -17,7 +17,16 @@ function M.filetypes_ext()
 		items[#items + 1] = ft
 	end
 	table.sort(items)
-	fzf_lua.fzf_exec(items, { prompt = "> " })
+	fzf_lua.fzf_exec(items, {
+		prompt = "> ",
+		actions = {
+			default = function(selecteds)
+				if not selecteds then return end
+				if not selecteds[1] then return end
+				vim.api.nvim_set_option_value("filetype", selecteds[1], { buf = 0 })
+			end,
+		},
+	})
 end
 
 return M
