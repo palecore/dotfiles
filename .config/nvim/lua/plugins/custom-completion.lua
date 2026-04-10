baad193886dfa068c551392cb69d42c76908a557
palecore <208073224+palecore@users.noreply.github.com>
2026-04-10 18:23:27 +0200

    fix: blink.cmp: use Tab as snippet-forward only if snippet is active
    This is to correct the default behavior when native Tab (increasing
    indentation / inserting a Tab character) gets hijacted by blink.cmp or
    luasnip to jump to a next snippet placeholder of a snippet that has
    already been escaped from.

--- .config/nvim/lua/plugins/custom-completion.lua
+++ .config/nvim/lua/plugins/custom-completion.lua
@@ -7,12 +7,20 @@ return {
 		"L3MON4D3/LuaSnip", -- for Lua snippets
 	},
 	config = function()
+		local function snip_forward_if_active(cmp)
+			return cmp.snippet_active() and cmp.snippet_forward() or nil
+		end
+		local function snip_backward_if_active(cmp)
+			return cmp.snippet_active() and cmp.snippet_backward() or nil
+		end
 		require("blink.cmp").setup({ ---@type blink.cmp.Config
 			keymap = {
 				["<CR>"] = { "accept", "fallback" },
 				["<C-e>"] = { "hide", "fallback" },
 				["<C-p>"] = { "select_prev", "fallback" },
 				["<C-n>"] = { "select_next", "fallback" },
+				["<Tab>"] = { snip_forward_if_active, "fallback" },
+				["<S-Tab>"] = { snip_backward_if_active, "fallback" },
 			},
 			cmdline = {
 				keymap = {
