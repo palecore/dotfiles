255657d839fa8a3f825ffd8a5cc3dfa9be70a02f
palecore <208073224+palecore@users.noreply.github.com>
2026-04-27 13:42:33 +0200

    feat: nvim: gitcommit filetype: loosen line restriction to 72 chars max
    What is most important in this restriction is to be able to put as many
    characters in the headline without GitHub truncating the line and
    putting the ellipsis symbol.

--- .config/nvim/after/ftplugin/gitcommit/50-palecore.lua
+++ .config/nvim/after/ftplugin/gitcommit/50-palecore.lua
@@ -1,2 +1,2 @@
-vim.opt_local.textwidth = 70 -- GitHub works best with lines at maximum 70
+vim.opt_local.textwidth = 72 -- GitHub works best with lines at maximum 72
 vim.opt_local.colorcolumn = { "+1", "81" } -- maximum of 80 is permittable
